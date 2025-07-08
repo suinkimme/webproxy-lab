77e9557ec5666fe2d78ff1cb5b435317932c05bd
Suin Kim <contact@suinkim.me>
2025-07-08 22:23:43 +0900

    proxy: main 함수 작성

--- webproxy-lab/proxy.c
+++ webproxy-lab/proxy.c
@@ -9,8 +9,25 @@ static const char *user_agent_hdr =
     "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 "
     "Firefox/10.0.3\r\n";
 
-int main()
-{
-  printf("%s", user_agent_hdr);
-  return 0;
+int main(int argc, char **argv) {
+  int listenfd, connfd;
+  char hostname[MAXLINE], port[MAXLINE];
+  socklen_t clientlen;
+  struct sockaddr_storage clientaddr;
+
+  if (argc != 2) {
+    fprintf(stderr, "usage: %s <port>\n", argv[0]);
+    exit(1);
+  }
+
+  listenfd = Open_listenfd(argv[1]);
+  while (1)
+  {
+    clientlen = sizeof(clientaddr);
+    connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
+    Getnameinfo((SA *)&clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE, 0);
+    printf("Accepted connection from (%s, %s)\n", hostname, port);
+    doit(connfd);
+    Close(connfd);
+  }
 }
